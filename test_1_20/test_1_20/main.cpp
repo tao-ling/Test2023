06a0b0f99a9cc7cff9ae1bff527f342ac4d4c0b0
lingtao <1310841339@qq.com>
2023-01-20 22:46:34 +0800

    first commit

---
+++ test_1_20/test_1_20/main.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+template <typename T>
+
+void myswap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+void test()
+{
+	int a = 10;
+	int b = 20;
+	//自动类型推导
+	//myswap(a, b);
+	//显式指定类型
+	myswap<int>(a, b);
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+}
+
+int main()
+{
+	test();
+	system("pause");
+	return 0;
+}
