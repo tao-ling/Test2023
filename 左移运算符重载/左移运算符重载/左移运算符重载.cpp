5ba3907775ebf483565567cce9f283d183755467
lingtao <1310841339@qq.com>
2023-02-20 23:41:39 +0800

    first commit

---
+++ 左移运算符重载/左移运算符重载/左移运算符重载.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+class Person
+{
+	friend ostream& operator<< (ostream& out, const Person& p);
+public:
+	Person(int a, int b)
+	{
+		this->m_A = a;
+		this->m_B = b;
+	}
+private:
+	int m_A;
+	int m_B;
+};
+
+ostream& operator<<(ostream& out, const Person& p)
+{
+	out << "m_A = " << p.m_A << "  m_B = " << p.m_B << endl;
+	return out;
+}
+
+void test()
+{
+	Person p1(10, 20);
+	cout << p1 << "hello world" << endl;
+}
+
+int main()
+{
+	test();
+	system("pause");
+	return 0;
+}
