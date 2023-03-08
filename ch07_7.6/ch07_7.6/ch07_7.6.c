f982bfe52260303ae59fbfedc87dacc29a0866be
lingtao <1310841339@qq.com>
2023-03-08 23:10:28 +0800

    first commit

---
+++ ch07_7.6/ch07_7.6/ch07_7.6.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdarg.h>		//通过宏实现可变参数列表
+
+
+/*
+	计算指定数量的值的平均值
+*/
+float average(int n_values, ...);
+
+int main(void) {
+	float ave = average(6, 1, 2, 3, 4, 5, 67);
+	printf("%f\n", ave);
+	return 0;
+}
+
+float average(int n_values, ...)		//需要传入可变参数列表中的参数个数
+{
+	va_list var_arg;		//声明用于访问参数列表的未确定部分变量的参数列表
+	int count;
+	float sum = 0;
+	va_start(var_arg, n_values);	//用va_start宏初始化参数列表
+	for (count = 0; count < n_values; count += 1) {
+		/*int i = va_arg(var_arg, int);
+		printf("%d ", i);*/
+		sum += va_arg(var_arg, int);	//使用va_arg宏和va_list变量来访问列表中的每一项
+	}
+	va_end(var_arg);	//完成功能后清理参数列表内存
+	return sum / n_values;
+}
---
+++ ch08_8.3/ch08_8.3/ch08_8.3.c
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+/*
+	判断参数是否与一个关键字列表中的任何单词匹配，
+	并返回匹配的索引值。如果未能找到匹配，函数返回-1
+*/
+#include <stdio.h>
+#include <string.h>
+#define N_KEYWORD (sizeof(keyword) / sizeof(keyword[0])
+
+const char* keyword[] = {
+	"do",
+	"for",
+	"if",
+	"register",
+	"return",
+	"switch",
+	"while",
+	NULL
+};
+
+
+int lookup_keyword(const char* desired_word,
+	const char* keyword_table[])
+{
+	const char** kwp;
+	for (kwp = keyword_table; *kwp != NULL; kwp++) {
+		/*
+			如果这个单词与我们所查找的单词匹配，返回它在表中的位置
+		*/
+		if (strcmp(desired_word, *kwp) == 0) {
+			return kwp - keyword_table;
+		}
+	}
+	return -1;
+}
+
+int main(void)
+{
+	const char* test = "whie";
+	int n = lookup_keyword(test, keyword);
+	printf("%d\n", n);
+	return 0;
+}
